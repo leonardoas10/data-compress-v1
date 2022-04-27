2061fce4880580d8841d90a61b2f23243b594c10
Leonardo Aranguren S <leoaranguren10@gmail.com>
2022-04-27 16:51:58 -0400

    Fix: delete-original-archives

--- lambdas/delete-original-archives/handler.py
+++ lambdas/delete-original-archives/handler.py
@@ -7,16 +7,13 @@ def handler(event, context):
         key = event['Records'][0]['s3']['object']['key']
         split = key.split(".")
         file_name = split[0]
-        # file = s3.Object('after-compress-files','colocolo-architecture.zip')
         original_bucket = s3.Bucket('before-compress-files')
-        # split = file.key.split(".")
-        # file_name = split[0]
 
         for f in original_bucket.objects.all():
             split_original_file = f.key.split(".")
             original_name = split_original_file[0]
             if original_name == file_name:
-                s3.delete_object('before-compress-files', f)
+                s3.Object('before-compress-files', f.key).delete()
                 print('Delete succesfull archive => ', f.key)      
     except Exception as e:
         print('Error handler() => ', e)
