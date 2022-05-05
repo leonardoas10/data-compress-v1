294d638aa6615e797b0e947708b3e35e756cca8c
Leonardo Aranguren S <leoaranguren10@gmail.com>
2022-05-05 19:56:15 -0400

    Fixing lamdas

--- lambdas/delete-final-archive-s3-and-update-db/handler.py
+++ lambdas/delete-final-archive-s3-and-update-db/handler.py
@@ -17,9 +17,9 @@ s3 = boto3.resource('s3')
 def handler(event, context):
     try:
         print('EVENT => ', event)
-        data = json.loads(event['body'])
+        # data = json.loads(event['body'])
 
-        sql_get_file="SELECT * from files WHERE id = {}".format(data['id'])
+        sql_get_file="SELECT * from files WHERE id = {}".format(event['id'])
         cursor.execute(sql_get_file)
         file = cursor.fetchall()[0]
         db.commit()
@@ -27,7 +27,7 @@ def handler(event, context):
 
         if not file['deleted_at']:
             s3.Object('after-compress-files', key).delete()
-            sql_update_file = "UPDATE files SET is_compress = 0, s3_url = NULL, deleted_at = CURRENT_TIMESTAMP WHERE id = {}".format(data['id'])
+            sql_update_file = "UPDATE files SET is_compress = 0, s3_url = NULL, deleted_at = CURRENT_TIMESTAMP WHERE id = {}".format(event['id'])
             cursor.execute(sql_update_file)
             db.commit()
 
@@ -36,7 +36,7 @@ def handler(event, context):
             'headers': {
                 'Access-Control-Allow-Origin': '*'
             },
-            'body': json.dumps({'ok': True, 'id': data['id']})
+            'body': json.dumps({'ok': True, 'id': event['id']})
         }
     except Exception as e:
         print('Error handler() => ', e)
