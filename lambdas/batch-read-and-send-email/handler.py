672a01d4fd21c2a1e184c196f51460c5eefccf38
Leonardo Aranguren S <leoaranguren10@gmail.com>
2022-05-09 11:50:26 -0400

    Adding: new lambda batch-read-and-send-email

--- lambdas/batch-read-and-send-email/handler.py
+++ lambdas/batch-read-and-send-email/handler.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 import os
 import mysql.connector
 import boto3
@@ -15,10 +14,8 @@ client = boto3.client('ses')
 
 def handler(event, context):
     try:
-        init_time = datetime.now()
-        print('Init Lambda => ', init_time)
 
-        sql_get_users="SELECT u.email from users u inner join files f on f.user_id = u.id WHERE f.is_compress = 1"
+        sql_get_users="SELECT u.email FROM users u inner join files f on f.user_id = u.id WHERE f.is_compress = 1 AND f.updated_at > DATE_SUB(NOW(), INTERVAL '1' HOUR) "
         cursor.execute(sql_get_users)
         users_db = cursor.fetchall()
         db.commit()
@@ -29,46 +26,41 @@ def handler(event, context):
             emails.append(user['email'])
 
         datas = []
+        fi = []
 
         for email in list(set(emails)): # delete duplicated users
-            sql_get_files="SELECT f.s3_url FROM users u  inner join files f on  f.user_id = u.id WHERE u.email = '{}'  AND f.is_compress = 1  ORDER BY f.updated_at DESC".format(email)
+            sql_get_files="SELECT f.s3_url FROM users u inner join files f on  f.user_id = u.id WHERE u.email = '{}'  AND f.is_compress = 1 AND f.updated_at > DATE_SUB(NOW(), INTERVAL '1' HOUR) ORDER BY f.updated_at DESC".format(email)
             cursor.execute(sql_get_files)
             files = cursor.fetchall()
             db.commit()
-            fi = []
             for file in files:
                 fi.append(file['s3_url'])
             datas.append({email: fi})
 
-        for data in datas:
-            email = list(data.keys())[0]
-            s3_urls = data[email]
-            template_data = ''
-            for s3_url in s3_urls:
-                template_data += "<li><a href='{}'>{}</a></li>".format(s3_url, s3_url)
+        if len(fi) > 0 :
+            for data in datas:
+                email = list(data.keys())[0]
+                s3_urls = data[email]
+                template_data = ''
+                for s3_url in s3_urls:
+                    template_data += "<li><a href='{}'>{}</a></li>".format(s3_url, s3_url)
 
-            #print(template_data)
-            #print(email)
+                order_template_data = '"URLS": "{}" '.format(template_data)
+                final_template_data_order = '{{{}}}'.format(order_template_data)
 
-            order_template_data = '"URLS": "{}" '.format(template_data)
-            order = '{{{}}}'.format(order_template_data)
-            # email_s = '{}'.format(email)
-            # print(email_s, email)
-            print('email =>' + email+"leo")
+                print('email =>' + email)
+                print('final_template_data_order => ', final_template_data_order)
 
-            client.send_templated_email(
-                Source='leoaranguren10@gmail.com',
-                Destination={
-                    'ToAddresses': [
-                        email
-                    ]
-                },
-                TemplateData= order,
-                #TemplateData='{"URLS": "<li> http </li><li> http </li><li> http </li><li> http </li>"}',
-                Template='COMPRESSES_FILES_IN_RANGE_OF_HOUR'
-            )
-        duration = datetime.now() - init_time
-        print('Finish Time in seconds => ', duration.total_seconds())
+                client.send_templated_email(
+                    Source=os.getenv("SENDER_EMAIL"),
+                    Destination={
+                        'ToAddresses': [
+                            email
+                        ]
+                    },
+                    TemplateData= final_template_data_order,
+                    Template='COMPRESSES_FILES_IN_RANGE_OF_HOUR'
+                )
 
     except Exception as e:
         print('Error => ', e)
